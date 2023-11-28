7d77947baf69de874bde5bb753af7a7bbc475ae1
rienvancampenhout <rien1van2campenhout3@gmail.com>
2023-11-28 11:54:28 +0100

    simplified some code in the ticketFactory

--- src/Tickets/TicketFactory.java
+++ src/Tickets/TicketFactory.java
@@ -11,13 +11,7 @@ public class TicketFactory {
 
     public Ticket createEvenlySplitTicket(String type, double price, Person person, ArrayList<Person> paidFor) {
         TicketDecorator decorator = new EvenlySplitDecorator(person, price, paidFor);
-        return switch (type) {
-            case "airplane" -> new AirplaneTicket(price, decorator, person);
-            case "concert" -> new ConcertTicket(price, decorator, person);
-            case "restaurant" -> new RestaurantTicket(price, decorator, person);
-            case "taxi" -> new TaxiTicket(price, decorator, person);
-            default -> new DefaultTicket(price, decorator, person);
-        };
+        return createTicket(type, person, price, decorator);
     }
 
     public Ticket createNotEvenlySplitTicket(String type, Person person, Map<Person,Double> detailedPaidFor) {
@@ -26,6 +20,10 @@ public class TicketFactory {
         for (double i : detailedPaidFor.values()) {
             price+=i;
         }
+        return createTicket(type, person, price, decorator);
+    }
+
+    private Ticket createTicket(String type, Person person, double price, TicketDecorator decorator) {
         return switch (type) {
             case "airplane" -> new AirplaneTicket(price, decorator, person);
             case "concert" -> new ConcertTicket(price, decorator, person);
