c41aee13d549f87227d8dafe80d0970a5dbcdf49
rienvancampenhout <rien1van2campenhout3@gmail.com>
2023-11-28 11:06:09 +0100

    Simplified some code in ticketFactory

--- src/Tickets/TicketFactory.java
+++ src/Tickets/TicketFactory.java
@@ -9,36 +9,38 @@ public class TicketFactory {
     public TicketFactory(){}
 
     public Ticket createEvenlySplitTicket(String type, double price, Person person, ArrayList<Person> paidFor) {
+        TicketDecorator decorator = new EvenlySplitDecorator(person, price, paidFor);
         switch (type) {
             case "airplane":
-                return new AirplaneTicket(price, new EvenlySplitDecorator(person, price, paidFor), person);
+                return new AirplaneTicket(price, decorator, person);
             case "concert":
-                return new ConcertTicket(price, new EvenlySplitDecorator(person, price, paidFor), person);
+                return new ConcertTicket(price, decorator, person);
             case "restaurant":
-                return new RestaurantTicket(price, new EvenlySplitDecorator(person, price, paidFor), person);
+                return new RestaurantTicket(price, decorator, person);
             case "taxi":
-                return new TaxiTicket(price, new EvenlySplitDecorator(person, price, paidFor), person);
+                return new TaxiTicket(price, decorator, person);
             default:
-                return new DefaultTicket(price, new EvenlySplitDecorator(person, price, paidFor), person);
+                return new DefaultTicket(price, decorator, person);
         }
     }
 
     public Ticket createNotEvenlySplitTicket(String type, Person person, Map<Person,Double> detailedPaidFor) {
         double price = 0;
+        TicketDecorator decorator = new NotEvenlySplitDecorator(person, detailedPaidFor);
         for (double i : detailedPaidFor.values()) {
             price+=i;
         }
         switch (type) {
             case "airplane":
-                return new AirplaneTicket(price, new NotEvenlySplitDecorator(person, detailedPaidFor), person);
+                return new AirplaneTicket(price, decorator, person);
             case "concert":
-                return new ConcertTicket(price, new NotEvenlySplitDecorator(person, detailedPaidFor), person);
+                return new ConcertTicket(price, decorator, person);
             case "restaurant":
-                return new RestaurantTicket(price, new NotEvenlySplitDecorator(person, detailedPaidFor), person);
+                return new RestaurantTicket(price, decorator, person);
             case "taxi":
-                return new TaxiTicket(price, new NotEvenlySplitDecorator(person, detailedPaidFor), person);
+                return new TaxiTicket(price, decorator, person);
             default:
-                return new DefaultTicket(price, new NotEvenlySplitDecorator(person, detailedPaidFor), person);
+                return new DefaultTicket(price, decorator, person);
             }
     }
 }
